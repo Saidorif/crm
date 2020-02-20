06b8b8b37b48df41382f690e69745de54a0fd9b9
Your Name <mirxan085@gmail.com>
2020-02-20 12:39:20 +0500

    vue is ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'material-icons/iconfont/material-icons.css';
+Vue.use(Vuesax)
+
+// Router
+import router from './routes'
+
+//Vuex
+import store from "./store/store"
+
+import Master from './components/layouts/Master'
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components:{
+      Master,
+    },
+	store,
+});
