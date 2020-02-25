a7b4c1407d3d7b78f68233bf1432787e413b269b
saidorif <sayyid2112@gmail.com>
2020-02-25 14:48:00 +0500

    srv

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/', 'HomeController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
