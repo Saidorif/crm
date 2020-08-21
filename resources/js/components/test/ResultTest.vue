9f19057ef37377fd8f96c0aa0c07f1a7657db6e3
Your Name <mirxan085@gmail.com>
2020-08-21 12:35:20 +0500

    all

--- resources/js/components/test/ResultTest.vue
+++ resources/js/components/test/ResultTest.vue
@@ -1,16 +1,16 @@
 <template>
 	<ul class="result_test user_info_result">
-		<li><b> All questions</b> {{items.wrong_answers + items.true_answers}} in percentage 100%</li>
+		<li><b> Все вопросы</b> {{items.wrong_answers + items.true_answers}} в процентах 100%</li>
 		<li>
-			<b>Wrong answers :</b> {{items.wrong_answers}} in percentage 
+			<b>Неправильные ответы:</b> {{items.wrong_answers ? items.wrong_answers : 0}} в процентах
 			{{countPercentage(items.wrong_answers)}}
 		</li>
 		<li>
-			<b>Right answers :</b> {{items.true_answers}} in percentage 
+			<b>Правильные ответы:</b> {{items.true_answers ? items.true_answers : 0}} в процентах
 			{{countPercentage(items.true_answers)}}
 		</li>
 		<router-link tag="button" class="btn btn-primary" :to='`/crm/test/test-result/${items.id}`'>
-			Show test result
+			Показать результат теста
 		</router-link>
 	</ul>
 </template>
@@ -28,7 +28,7 @@
 			countPercentage(number){
 				let summ = this.items.wrong_answers + this.items.true_answers
 				let result = Math.round((number*100)/summ);
-				return result + ' %';
+				return result ? result : 0 + ' %';
 			}
 		}
 		
